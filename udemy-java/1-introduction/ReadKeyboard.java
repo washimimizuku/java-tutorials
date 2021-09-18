ab8f6e2b57aa21d8e31dd03bd241b7ddea416ea4
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-18 17:52:14 +0200

    Add scanner example

--- udemy-java/1-introduction/ReadKeyboard.java
+++ udemy-java/1-introduction/ReadKeyboard.java
@@ -27,6 +27,11 @@ public class ReadKeyboard
         int z = x + y;
         System.out.println("Sum is " + z);
 
+        System.out.println("Enter a binary number: ");
+        sc.useRadix(2);
+        int binary = sc.nextInt();
+        System.out.println(binary);
+
         System.out.print("String word: ");
         String d = sc.next();
         System.out.println(d);
