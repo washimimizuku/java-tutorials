a405240fda2c5f691c6290d6b8e59a96b4852937
Nuno Barreto <nuno.barreto@inventsys.ch>
2021-09-18 17:48:55 +0200

    Add scanner example

---
+++ udemy-java/1-introduction/ReadKeyboard.java
@@ -0,0 +1,38 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class ReadKeyboard
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("String line: ");
+        String c = sc.nextLine();
+        System.out.println(c);
+
+        System.out.print("Integer: ");
+        int a = sc.nextInt();
+        System.out.println(a);
+
+        System.out.print("Float: ");
+        float b = sc.nextFloat();
+        System.out.println(b);
+
+        int x, y;
+        System.out.println("Enter 2 numbers: ");
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        int z = x + y;
+        System.out.println("Sum is " + z);
+
+        System.out.print("String word: ");
+        String d = sc.next();
+        System.out.println(d);
+
+        sc.close();
+
+    }
+    
+}
